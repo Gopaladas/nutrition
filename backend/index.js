72368b314e9e5a8e32ef5867822e97133145ffdf
pavangopaldas <pavangopaldas@gmail.com>
2025-10-23 18:11:35 +0530

    added frontend api

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ dotenv.config();
 import cors from "cors";
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    // origin: "http://localhost:5173",
+    origin: "https://nutrition-frontend-lac.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // allow cookies
